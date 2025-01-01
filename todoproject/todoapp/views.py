a61a8a6d7d8e191991da6f28fcd144c549ecdf91
jeremyabeard5 <jeremyab5@gmail.com>
2025-01-01 13:49:17 -0700

    fixed weird csrf issue with additional explicit calls. added initial about.html template

--- todoproject/todoapp/views.py
+++ todoproject/todoapp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.middleware.csrf import get_token
+#from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from .models import Todo, BlogPost, Media
 
@@ -6,31 +8,42 @@ from .models import Todo, BlogPost, Media
 
 ## Render the home page
 def index(request):
+    print("views.py: index")
     return render(request, 'index.html')
 
 ## Render the todo page
 def todo(request):
+    print("views.py: todo")
     return render(request, 'todo.html')
 
 ## Render the contact page
 def contact(request):
+    print("views.py: contact")
     return render(request, 'contact.html')
 
 ## Render the services page
 def services(request):
+    print("views.py: services")
     return render(request, 'services.html')
 
 ## Render the Tableau page
 def tableau(request):
+    print("views.py: tableau")
     return render(request, 'tableau.html')
 
 ## Get all todos
 def get_todos(request):
+    print("views.py: get_todos")
     todos = list(Todo.objects.values())
     return JsonResponse({'todos': todos})
 
 ## Create a new todo
+#@csrf_exempt
 def add_todo(request):
+    print("views.py: add_todo")
+    csrf_token = get_token(request)
+    print("CSRF Token from middleware:", csrf_token)
+    print("CSRF Token from cookie:", request.COOKIES.get('csrftoken'))
     if request.method == 'POST':
         title = request.POST.get('title', '')
         todo = Todo.objects.create(title=title)
@@ -40,6 +53,7 @@ def add_todo(request):
 
 ## Update a todo
 def update_todo(request, id):
+    print("views.py: update_todo")
     if request.method == 'POST':
         todo = Todo.objects.get(id=id)
         todo.is_completed = not todo.is_completed
@@ -50,6 +64,7 @@ def update_todo(request, id):
 
 ## Delete a todo
 def delete_todo(request, id):
+    print("views.py: delete_todo")
     if request.method == 'POST':
         todo = Todo.objects.get(id=id)
         todo.delete()
@@ -59,14 +74,17 @@ def delete_todo(request, id):
 
 # List of all blog posts
 def blog_list(request):
+    print("views.py: blog_list")
     posts = BlogPost.objects.all().order_by('-created_at')
     return render(request, 'blog_list.html', {'posts': posts})
 
 # Single blog post detail view
 def blog_detail(request, id):
+    print("views.py: blog_detail")
     post = get_object_or_404(BlogPost, id=id)
     return render(request, 'blog_detail.html', {'post': post})
 
 def media_feed(request):
+    print("views.py: media_feed")
     photos = Media.objects.all().order_by('-uploaded_at')  # Latest first
     return render(request, 'media_feed.html', {'photos': photos})
